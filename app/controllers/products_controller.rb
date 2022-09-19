e0b43ae07507b12072cb77e3b53b697e60029e16
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-09-19 14:58:34 +0200

    try to make a post request...

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,15 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def create
+    @product = Product.create(product_params)
+    render json: @product
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name)
+  end
 end
--- app/javascript/cart/components/cart_show.jsx
+++ app/javascript/cart/components/cart_show.jsx
@@ -3,12 +3,52 @@ import { connect } from 'react-redux';
 import ProductShow from '../components/product_show'
 
 class CartShow extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      input: null
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const url = `/products`;
+    fetch(url, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {product: JSON.stringify(e.target.value)},
+    })
+      .then((data) => {
+        if (data.ok) {
+          // this.reloadBeers();
+          return data.json();
+        }
+        throw new Error("Network error.");
+      })
+      .catch((err) => message.error("Error: " + err));
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value
+    })
+  }
 
   render () {
     if (this.props.selectedCart) {
       const dateString = new Date(this.props.selectedCart.created_at).toDateString();
       return  <div>
                 <h4 id={this.props.selectedCart.id}>{dateString}</h4>
+                <form onSubmit={this.handleSubmit}>
+                  <label>
+                    Nom:
+                    <input type="text" onChange={this.handleChange}/>
+                  </label>
+                  <input type="submit" value="Submit" className='btn btn-primary' />
+                </form>
                 {this.props.selectedProducts.map(product => <ProductShow product={product} key= {product.id}/>)}
               </div>
     }
