fb757edca84a6e83b91d80e90488570c27a67620
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-09-20 18:09:21 +0200

    update new product

--- app/javascript/cart/components/product_show.jsx
+++ app/javascript/cart/components/product_show.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
 
 export default class ProductShow extends React.Component {
 
