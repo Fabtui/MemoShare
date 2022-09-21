e337b2c84189fbd7cd49066e31e30251f2051180
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-09-21 19:50:41 +0200

    clean appliation.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,4 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import "../controllers"
+import "./controllers"
 import "bootstrap"
-import Rails from "@rails/ujs"
-// import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-// import "channels"
-
-Rails.start()
-// Turbolinks.start()
-ActiveStorage.start()
